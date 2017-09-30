d12f41deca760095a25d8cc23be5735421ed8985
JC <simocl@yahoo.fr>
2017-09-30 11:47:46 -0400

    Mise à jour structure projet

---
+++ FreneValue/obj/Release/Package/PackageTmp/Scripts/autrescript.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="jquery-ui-1.12.1.js" />
+/// <reference path="jquery.validate-vsdoc.js" />
+
+$(document).ready(function () {
+
+    $(":input[data-autocomplete]").each(function () {
+        $(this).autocomplete({ source: $(this).attr("data-autocomplete") });
+    });
+
+    $(":input[data-datepicker]").datepicker();
+
+
+})
